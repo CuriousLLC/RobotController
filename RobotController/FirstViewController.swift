e824f55d5c4342bfa8e8c4e5f7963c0a3b4dbc01
Ryan Day <ryan@ryanday.net>
2015-11-24 20:52:23 -0500

    Initial Commit

---
+++ RobotController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RobotController
+//
+//  Created by ryan day on 11/24/15.
+//  Copyright © 2015 Curious. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
